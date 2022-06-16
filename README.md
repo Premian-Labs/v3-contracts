77fd68ba8514c0fee018480bcd28b87b9e322bb7
Lumyo <lumyo.b@gmail.com>
2022-06-16 13:06:49 +0200

    Init

---
+++ README.md
@@ -0,0 +1,72 @@
+# Premia - Next-Generation Options AMM
+
+https://premia.finance
+
+## Deployments
+TBD
+<!---
+| Network          |                                      |
+| ---------------- | ------------------------------------ |
+| Ethereum Mainnet | [📜](./docs/deployments/ETHEREUM.md) |
+| Arbitrum Mainnet | [📜](./docs/deployments/ARBITRUM.md) |
+| Fantom           | [📜](./docs/deployments/FANTOM.md)   |
+-->
+
+## Development
+
+Install dependencies via Yarn:
+
+```bash
+yarn install
+```
+
+Setup Husky to format code on commit:
+
+```bash
+yarn prepare
+```
+
+Create a `.env` file with the following values defined:
+
+| Key                 | Description                                                              |
+| ------------------- | ------------------------------------------------------------------------ |
+| `API_KEY_ALCHEMY`   | [Alchemy](https://www.alchemy.com/) API key for node connectivity        |
+| `API_KEY_ETHERSCAN` | [Etherscan](https://etherscan.io//) API key for source code verification |
+| `PKEY_ETH_TEST`     | private key for test/development use on testnets                         |
+| `FORK_MODE`         | if `true`, the local Hardhat network will be forked from mainnet         |
+| `REPORT_GAS`        | if `true`, a gas report will be generated after running tests            |
+
+Create a `.env.secret` file with the following values defined:
+
+| Key             | Description                                |
+| --------------- | ------------------------------------------ |
+| `PKEY_ETH_MAIN` | private key for production use on mainnets |
+
+### Testing
+
+Test contracts via Hardhat:
+
+```bash
+yarn run hardhat test
+```
+
+Activate gas usage reporting by setting the `REPORT_GAS` environment variable to `"true"`:
+
+```bash
+REPORT_GAS=true yarn run hardhat test
+```
+
+Generate a code coverage report using `solidity-coverage`:
+
+```bash
+yarn run hardhat coverage
+```
+
+## Licensing
+
+The primary license for Premia contracts is the Business Source License 1.1 (`BUSL-1.1`), see [`LICENSE`](./LICENSE).
+
+### Exceptions
+
+- Interfaces are licensed under `LGPL-3.0-or-later` (as indicated in their SPDX headers), see [`LICENSE_LGPL`](./LICENSE_LGPL)
+- All files in `contracts/test` remain unlicensed.
---
+++ hardhat.config.ts
@@ -0,0 +1,163 @@
+import Dotenv from 'dotenv';
+// Hardhat plugins
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-etherscan';
+import '@solidstate/hardhat-4byte-uploader';
+import '@solidstate/hardhat-test-short-circuit';
+import '@typechain/hardhat';
+import 'hardhat-abi-exporter';
+import 'hardhat-artifactor';
+import 'hardhat-contract-sizer';
+import 'hardhat-dependency-compiler';
+import 'hardhat-docgen';
+import 'hardhat-gas-reporter';
+import 'hardhat-spdx-license-identifier';
+import 'solidity-coverage';
+// tasks and task overrides
+import './tasks/accounts';
+import './tasks/typechain_generate_types';
+
+Dotenv.config();
+
+const {
+  API_KEY_ALCHEMY,
+  API_KEY_ETHERSCAN,
+  API_KEY_ARBISCAN,
+  API_KEY_FTMSCAN,
+  PKEY_ETH_MAIN,
+  PKEY_ETH_TEST,
+  FORK_MODE,
+  FORK_BLOCK_NUMBER,
+  REPORT_GAS,
+  CACHE_PATH,
+} = process.env;
+
+export default {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.15',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  paths: {
+    cache: CACHE_PATH ?? './cache',
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+      blockGasLimit: 180000000000,
+      ...(FORK_MODE === 'true'
+        ? {
+            forking: {
+              url: `https://eth-mainnet.alchemyapi.io/v2/${API_KEY_ALCHEMY}`,
+              blockNumber: parseInt(FORK_BLOCK_NUMBER ?? '13717777'),
+            },
+          }
+        : {}),
+    },
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${API_KEY_ALCHEMY}`,
+      accounts: [PKEY_ETH_MAIN],
+      //gas: 120000000000,
+      // blockGasLimit: 120000000000,
+      // gasPrice: 100000000000,
+      timeout: 100000,
+    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${API_KEY_ALCHEMY}`,
+      accounts: [PKEY_ETH_TEST],
+      //gas: 120000000000,
+      blockGasLimit: 120000000000,
+      //gasPrice: 10,
+      timeout: 300000,
+    },
+    kovan: {
+      url: `https://eth-kovan.alchemyapi.io/v2/${API_KEY_ALCHEMY}`,
+      accounts: [PKEY_ETH_TEST],
+      //gas: 120000000000,
+      blockGasLimit: 120000000000,
+      //gasPrice: 10,
+      timeout: 300000,
+    },
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${API_KEY_ALCHEMY}`,
+      accounts: [PKEY_ETH_TEST],
+      //gas: 120000000000,
+      blockGasLimit: 120000000000,
+      //gasPrice: 10,
+      timeout: 300000,
+    },
+    arbitrum: {
+      url: `https://arb1.arbitrum.io/rpc`,
+      accounts: [PKEY_ETH_MAIN],
+      //gas: 120000000000,
+      // blockGasLimit: 120000000000,
+      //gasPrice: 10,
+      timeout: 300000,
+    },
+    rinkebyArbitrum: {
+      url: `https://rinkeby.arbitrum.io/rpc`,
+      accounts: [PKEY_ETH_TEST],
+      //gas: 120000000000,
+      // blockGasLimit: 120000000000,
+      // gasPrice: 100000000000,
+      timeout: 100000,
+    },
+    fantomDev: {
+      url: `https://rpc.ftm.tools/`,
+      accounts: [PKEY_ETH_TEST],
+      timeout: 100000,
+    },
+    fantom: {
+      url: `https://rpc.ftm.tools/`,
+      accounts: [PKEY_ETH_MAIN],
+      timeout: 100000,
+    },
+  },
+
+  abiExporter: {
+    runOnCompile: false,
+    path: './abi',
+    clear: true,
+    flat: true,
+    except: ['@uniswap'],
+  },
+
+  docgen: {
+    runOnCompile: false,
+    clear: true,
+  },
+
+  etherscan: {
+    apiKey: {
+      mainnet: API_KEY_ETHERSCAN,
+      arbitrumOne: API_KEY_ARBISCAN,
+      opera: API_KEY_FTMSCAN,
+    },
+  },
+
+  gasReporter: {
+    enabled: REPORT_GAS === 'true',
+  },
+
+  spdxLicenseIdentifier: {
+    overwrite: false,
+    runOnCompile: true,
+  },
+
+  typechain: {
+    alwaysGenerateOverloads: true,
+    outDir: 'typechain',
+  },
+
+  mocha: {
+    timeout: 60000,
+  },
+};
