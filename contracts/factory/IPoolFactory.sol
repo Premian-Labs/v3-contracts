610d5570553b0105060b34ae9a3c153bab2a391a
Lumyo <lumyo.b@gmail.com>
2022-09-21 17:11:34 +0200

    Update `deployPool`

--- contracts/factory/IPoolFactory.sol
+++ contracts/factory/IPoolFactory.sol
@@ -3,6 +3,15 @@
 pragma solidity ^0.8.0;
 
 interface IPoolFactory {
+    event PairCreated(
+        address indexed base,
+        address indexed underlying,
+        address baseOracle,
+        address underlyingOracle,
+        address callPool,
+        address putPool
+    );
+
     function deployPool(
         address base,
         address underlying,
