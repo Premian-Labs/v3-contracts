ff2d4344626b98d1886b4d5b8dedd91713f9186f
Nick Barry <itsnickbarry@protonmail.ch>
2022-10-31 22:40:22 -0600

    use UintUtils library for addition of signed integers

--- contracts/pool/PoolTicks.sol
+++ contracts/pool/PoolTicks.sol
@@ -4,6 +4,7 @@
 pragma solidity ^0.8.0;
 
 import {Math} from "@solidstate/contracts/utils/Math.sol";
+import {UintUtils} from "@solidstate/contracts/utils/UintUtils.sol";
 import {SafeCast} from "@solidstate/contracts/utils/SafeCast.sol";
 
 import {IPoolTicks} from "./IPoolTicks.sol";
@@ -22,6 +23,7 @@ contract PoolTicks is IPoolTicks {
     using Math for uint256;
     using Math for int256;
     using SafeCast for uint256;
+    using UintUtils for uint256;
 
     error PoolTicks__InvalidInsertLocation();
     error PoolTicks__InvalidInsert();
@@ -118,7 +120,7 @@ contract PoolTicks is IPoolTicks {
             if (position.rangeSide == PoolStorage.Side.SELL) {
                 if (position.lower == l.marketPrice) {
                     l.ticks[lower] = l.ticks[lower].cross(l.globalFeeRate);
-                    l.liquidityRate = l.liquidityRate - Math.abs(delta);
+                    l.liquidityRate = l.liquidityRate.add(delta);
 
                     if (l.tick < position.lower) l.tick = lower;
                 }
@@ -134,7 +136,7 @@ contract PoolTicks is IPoolTicks {
             if (position.rangeSide == PoolStorage.Side.BUY) {
                 l.ticks[upper] = l.ticks[upper].cross(l.globalFeeRate);
                 if (l.tick <= position.upper) {
-                    l.liquidityRate = l.liquidityRate - Math.abs(delta);
+                    l.liquidityRate = l.liquidityRate.add(delta);
                 }
                 if (l.tick < position.lower) {
                     l.tick = lower;
