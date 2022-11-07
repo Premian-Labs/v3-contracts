a1fcb422429288d571acaadbf0ccb5a94c16fe2f
Lumyo <lumyo.b@gmail.com>
2022-11-07 23:43:19 +0100

    Remove license clarification link

--- contracts/factory/PoolFactory.sol
+++ contracts/factory/PoolFactory.sol
@@ -1,9 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 
-
-
-// For further clarification please see https://license.premia.legal
-
 pragma solidity ^0.8.0;
 
 import {IPoolFactory} from "./IPoolFactory.sol";
--- contracts/libraries/Position.sol
+++ contracts/libraries/Position.sol
@@ -1,9 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 
-
-
-// For further clarification please see https://license.premia.legal
-
 pragma solidity ^0.8.0;
 
 import {Math} from "@solidstate/contracts/utils/Math.sol";
