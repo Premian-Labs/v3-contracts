094c006094d4575a4f210c12c3bcd6f2635c4f02
Lumyo <lumyo@proton.me>
2022-12-17 13:55:45 +0100

    Update Position.sol

--- contracts/libraries/Position.sol
+++ contracts/libraries/Position.sol
@@ -28,6 +28,7 @@ library Position {
         uint256 lower;
         // The upper tick normalized price of the range order
         uint256 upper;
+        OrderType orderType;
         // ---- Values under are not used to compute the key hash but are included in this struct to reduce stack depth
         uint256 strike;
         bool isCall;
@@ -36,15 +37,23 @@ library Position {
     // All the data required to be saved in storage
     struct Data {
         // The amount of ask (bid) collateral the LP provides
-        uint256 collateral;
+        uint256 collateral; // ToDo : Remove
         // The amount of long (short) contracts the LP provides
-        uint256 contracts;
+        uint256 contracts; // ToDo : Remove
+        uint256 initialAmount;
         // Used to track claimable fees over time
         uint256 lastFeeRate;
         // The amount of fees a user can claim now. Resets after claim
         uint256 claimableFees;
         // Whether side is BUY or SELL
-        bool isBuy;
+        bool isBuy; // ToDo : Remove
+    }
+
+    enum OrderType {
+        BUY_WITH_COLLATERAL,
+        BUY_WITH_SHORTS,
+        SELL_WITH_COLLATERAL,
+        SELL_WITH_LONGS
     }
 
     struct Liquidity {
@@ -56,8 +65,38 @@ library Position {
     function keyHash(Key memory self) internal pure returns (bytes32) {
         return
             keccak256(
-                abi.encode(self.owner, self.operator, self.lower, self.upper)
+                abi.encode(
+                    self.owner,
+                    self.operator,
+                    self.lower,
+                    self.upper,
+                    self.orderType
+                )
             );
+    }
+
+    function opposite(OrderType orderType) internal pure returns (OrderType) {
+        if (orderType == OrderType.BUY_WITH_COLLATERAL)
+            return OrderType.SELL_WITH_LONGS;
+        else if (orderType == OrderType.SELL_WITH_LONGS)
+            return OrderType.BUY_WITH_COLLATERAL;
+        else if (orderType == OrderType.BUY_WITH_SHORTS)
+            return OrderType.SELL_WITH_COLLATERAL;
+        else if (orderType == OrderType.SELL_WITH_COLLATERAL)
+            return OrderType.BUY_WITH_SHORTS;
+        else revert();
+    }
+
+    function isLeft(OrderType orderType) internal pure returns (bool) {
+        return
+            orderType == OrderType.BUY_WITH_SHORTS ||
+            orderType == OrderType.BUY_WITH_COLLATERAL;
+    }
+
+    function isRight(OrderType orderType) internal pure returns (bool) {
+        return
+            orderType == OrderType.SELL_WITH_COLLATERAL ||
+            orderType == OrderType.SELL_WITH_LONGS;
     }
 
     function proportion(
@@ -70,20 +109,30 @@ library Position {
         return 1e18;
     }
 
+    function pieceWiseLinear(
+        Key memory self,
+        uint256 price
+    ) internal pure returns (uint256) {
+        if (price < self.lower) return 0;
+        else if (self.lower <= price && price < self.upper)
+            return self.proportion(price);
+        else return 1e18;
+    }
+
     function collateralToContracts(
-        uint256 collateral,
+        uint256 _collateral,
         uint256 strike,
         bool isCall
     ) internal pure returns (uint256) {
-        return isCall ? collateral : collateral.divWad(strike);
+        return isCall ? _collateral : _collateral.divWad(strike);
     }
 
     function contractsToCollateral(
-        uint256 contracts,
+        uint256 _contracts,
         uint256 strike,
         bool isCall
     ) internal pure returns (uint256) {
-        return isCall ? contracts : contracts.mulWad(strike);
+        return isCall ? _contracts : _contracts.mulWad(strike);
     }
 
     function averagePrice(Key memory self) internal pure returns (uint256) {
@@ -94,16 +143,18 @@ library Position {
         Key memory self,
         Data memory data
     ) internal pure returns (uint256 contractsLiquidity) {
-        contractsLiquidity = collateralToContracts(
-            data.collateral,
-            self.strike,
-            self.isCall
-        );
+        if (self.orderType == OrderType.SELL_WITH_COLLATERAL) {
+            return data.initialAmount.divWad(1e18 - self.averagePrice());
+        } else if (self.orderType == OrderType.BUY_WITH_COLLATERAL) {
+            return data.initialAmount.divWad(self.averagePrice());
+        } else if (
+            self.orderType == OrderType.SELL_WITH_LONGS ||
+            self.orderType == OrderType.BUY_WITH_SHORTS
+        ) {
+            return data.initialAmount;
+        }
 
-        return
-            data.isBuy
-                ? contractsLiquidity.divWad(self.averagePrice())
-                : contractsLiquidity + data.contracts;
+        revert();
     }
 
     /// @notice Returns the per-tick liquidity phi (delta) for a specific position.
@@ -131,31 +182,46 @@ library Position {
         return data.isBuy ? self.upper - shift : self.lower + shift;
     }
 
-    /// @notice  nu = proportion(p, p^*, p_upper)
-    ///   Right-side:
-    ///       Amount of bid-side collateral generated through underwriting
-    ///       options (the revenue generated through premiums collected) and
-    ///       closing long positions.
-    ///
-    ///           bid(p) = nu * (c + d) * mu(p_lower, p_upper)
-    ///
-    ///   Left-side:
-    ///       The amount of bid-side collateral remaining.
-    ///
-    ///           bid(p) = nu * c
+    /// @notice Represents the total amount of bid liquidity the position is holding
+    /// at a particular price. In other words, it is the total amount of buying
+    /// power the position has at the current price.
+    function bidLiquidity(
+        Key memory self,
+        Data memory data,
+        uint256 price
+    ) internal pure returns (uint256) {
+        return self.pieceWiseLinear(price).mulWad(self.liquidity(data));
+    }
+
+    /// @notice Represents the total amount of ask liquidity the position is holding
+    /// at a particular price. In other words, it is the total amount of
+    /// selling power the position has at the current price.
+    /// Can also be computed as,
+    ///     total_bid(p) = ask(p) + long(p)
+    function askLiquidity(
+        Key memory self,
+        Data memory data,
+        uint256 price
+    ) internal pure returns (uint256) {
+        return
+            (1e18 - self.pieceWiseLinear(price)).mulWad(self.liquidity(data));
+    }
+
+    /// @notice Bid collateral either used to buy back options or revenue /
+    ///         income generated from underwriting / selling options.
     function bid(
         Key memory self,
         Data memory data,
         uint256 price
     ) internal pure returns (uint256) {
-        uint256 nu = self.proportion(price);
+        uint256 nu = self.pieceWiseLinear(price);
 
-        // Overworked to avoid numerical rounding errors
+        // overworked to avoid numerical rounding errors
         uint256 quotePrice;
         if (price < self.lower) {
             quotePrice = self.lower;
         } else if (self.lower <= price && price < self.upper) {
-            quotePrice = (price + self.lower) / 2;
+            quotePrice = Math.average(price, self.lower);
         } else {
             quotePrice = self.averagePrice();
         }
@@ -165,6 +231,7 @@ library Position {
         return contractsToCollateral(result, self.strike, self.isCall);
     }
 
+    // ToDo : Remove
     /// @notice
     ///    nu = proportion(p, p^*, p_upper)
     ///    Right-side:
@@ -186,89 +253,121 @@ library Position {
         return (1e18 - nu).mulWad(x);
     }
 
-    /// @notice nu = proportion(p, p_lower, p^*)
-    ///    Right-side:     long(p) = (1 - nu) *  d
-    ///    Left-side:      long(p) = (1 - nu) * (c/mu - d)
+    /// @notice Total collateral held by the position. Note that here we do not
+    ///         distinguish between ask- and bid-side collateral. This increases the
+    ///         capital efficiency of the range order.
+    function collateral(
+        Key memory self,
+        Data memory data,
+        uint256 price
+    ) internal pure returns (uint256 _collateral) {
+        uint256 nu = pieceWiseLinear(self, price);
+
+        if (
+            self.orderType == OrderType.SELL_WITH_COLLATERAL ||
+            self.orderType == OrderType.BUY_WITH_SHORTS
+        ) {
+            _collateral = (1e18 - nu).mulWad(self.liquidity(data));
+        } else if (
+            self.orderType == OrderType.BUY_WITH_COLLATERAL ||
+            self.orderType == OrderType.SELL_WITH_LONGS
+        ) {
+            _collateral = self.bid(data, price);
+        } else {
+            revert();
+        }
+    }
+
+    function contracts(
+        Key memory self,
+        Data memory data,
+        uint256 price
+    ) internal pure returns (uint256) {
+        uint256 nu = pieceWiseLinear(self, price);
+
+        if (
+            self.orderType == OrderType.SELL_WITH_LONGS ||
+            self.orderType == OrderType.BUY_WITH_COLLATERAL
+        ) {
+            return (1e18 - nu).mulWad(self.liquidity(data));
+        }
+
+        return nu.mulWad(self.liquidity(data));
+    }
+
+    /// @notice Number of long contracts held in position at current price
     function long(
         Key memory self,
         Data memory data,
         uint256 price
     ) internal pure returns (uint256) {
-        uint256 nu = self.proportion(price);
-        uint256 x = data.isBuy
-            ? data.contracts
-            : collateralToContracts(data.collateral, self.strike, self.isCall);
-        return (1e18 - nu).mulWad(self.liquidity(data) - x);
+        if (
+            self.orderType == OrderType.SELL_WITH_COLLATERAL ||
+            self.orderType == OrderType.BUY_WITH_SHORTS
+        ) {
+            return 0;
+        } else if (
+            self.orderType == OrderType.BUY_WITH_COLLATERAL ||
+            self.orderType == OrderType.SELL_WITH_LONGS
+        ) {
+            return self.contracts(data, price);
+        } else {
+            revert();
+        }
     }
 
+    /// @notice Number of short contracts held in position at current price
     function short(
         Key memory self,
         Data memory data,
         uint256 price
     ) internal pure returns (uint256) {
-        uint256 nu = self.proportion(price);
-        uint256 x = data.isBuy
-            ? data.contracts
-            : collateralToContracts(data.collateral, self.strike, self.isCall);
-        return nu.mulWad(x);
-    }
-
-    /// @notice Represents the total amount of bid liquidity the position is holding
-    /// at a particular price. In other words, it is the total amount of buying
-    /// power the position has at the current price.
-    function bidLiquidity(
-        Key memory self,
-        Data memory data,
-        uint256 price
-    ) internal pure returns (uint256) {
-        uint256 nu = self.proportion(price);
-        return nu.mulWad(self.liquidity(data));
-    }
-
-    /// @notice Represents the total amount of ask liquidity the position is holding
-    /// at a particular price. In other words, it is the total amount of
-    /// selling power the position has at the current price.
-    /// Can also be computed as,
-    ///     total_bid(p) = ask(p) + long(p)
-    function askLiquidity(
-        Key memory self,
-        Data memory data,
-        uint256 price
-    ) internal pure returns (uint256) {
-        uint256 nu = self.proportion(price);
-        return (1e18 - nu).mulWad(self.liquidity(data));
+        if (
+            self.orderType == OrderType.SELL_WITH_COLLATERAL ||
+            self.orderType == OrderType.BUY_WITH_SHORTS
+        ) {
+            return self.contracts(data, price);
+        } else if (
+            self.orderType == OrderType.BUY_WITH_COLLATERAL ||
+            self.orderType == OrderType.SELL_WITH_LONGS
+        ) {
+            return 0;
+        } else {
+            revert();
+        }
     }
 
     function assertSufficientBidLiquidity(
         Key memory self,
         Data memory data,
-        uint256 collateral,
-        uint256 contracts,
+        uint256 _collateral,
+        uint256 _contracts,
         bool withdrawal
     ) internal pure {
-        collateral = withdrawal
-            ? data.collateral - collateral
-            : data.collateral + collateral;
-        contracts = withdrawal
-            ? data.contracts - contracts
-            : data.contracts + contracts;
+        _collateral = withdrawal
+            ? data.collateral - _collateral
+            : data.collateral + _collateral;
+        _contracts = withdrawal
+            ? data.contracts - _contracts
+            : data.contracts + _contracts;
 
         if (
-            collateral <
+            _collateral <
             contractsToCollateral(self.averagePrice(), self.strike, self.isCall)
-                .mulWad(contracts)
+                .mulWad(_contracts)
         ) revert Position__InsufficientBidLiquidity();
     }
 
     function assertSufficientFunds(
         Data memory data,
-        uint256 collateral,
-        uint256 contracts
+        uint256 _collateral,
+        uint256 _contracts
     ) internal pure {
-        if (collateral > data.collateral || contracts > data.contracts)
+        if (_collateral > data.collateral || _contracts > data.contracts)
             revert Position__InsufficientFunds();
     }
 
+    // ToDo : Update
     /// @notice Convert position to opposite side to make it modifiable. A position is
     ///    modifiable if it's side does not need updating.
     function flipSide(
