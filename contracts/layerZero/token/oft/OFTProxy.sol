ac373a7ecabf0d944b363d2ea7b584996f9ad59a
Nick Barry <itsnickbarry@protonmail.ch>
2022-12-29 19:30:20 +0200

    update contracts to comply with new Solidstate patterns

--- contracts/layerZero/token/oft/OFTProxy.sol
+++ contracts/layerZero/token/oft/OFTProxy.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import {ERC165Storage} from "@solidstate/contracts/introspection/ERC165Storage.sol";
+import {ERC165BaseInternal} from "@solidstate/contracts/introspection/ERC165/base/ERC165BaseInternal.sol";
 import {IERC165} from "@solidstate/contracts/interfaces/IERC165.sol";
 import {IERC20} from "@solidstate/contracts/interfaces/IERC20.sol";
 
@@ -10,18 +10,15 @@ import {ProxyUpgradeableOwnable} from "../../../proxy/ProxyUpgradeableOwnable.so
 import {IOFT} from "./IOFT.sol";
 import {IOFTCore} from "./IOFTCore.sol";
 
-contract OFTProxy is ProxyUpgradeableOwnable {
-    using ERC165Storage for ERC165Storage.Layout;
-
+contract OFTProxy is ProxyUpgradeableOwnable, ERC165BaseInternal {
     constructor(
         address implementation
     ) ProxyUpgradeableOwnable(implementation) {
         {
-            ERC165Storage.Layout storage l = ERC165Storage.layout();
-            l.setSupportedInterface(type(IERC165).interfaceId, true);
-            l.setSupportedInterface(type(IERC20).interfaceId, true);
-            l.setSupportedInterface(type(IOFTCore).interfaceId, true);
-            l.setSupportedInterface(type(IOFT).interfaceId, true);
+            _setSupportsInterface(type(IERC165).interfaceId, true);
+            _setSupportsInterface(type(IERC20).interfaceId, true);
+            _setSupportsInterface(type(IOFTCore).interfaceId, true);
+            _setSupportsInterface(type(IOFT).interfaceId, true);
         }
     }
 }
