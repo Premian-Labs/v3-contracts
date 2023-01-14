e34c20b52e2903fd71701d20e4945c5a4c625810
Froggie <froggie@premia.finance>
2023-01-14 10:24:55 -1000

    Add ERC1155 interfaces to Pool

--- contracts/pool/IPoolBase.sol
+++ contracts/pool/IPoolBase.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 
+import {IERC1155} from "@solidstate/contracts/interfaces/IERC1155.sol";
+import {IERC1155Enumerable} from "@solidstate/contracts/token/ERC1155/enumerable/IERC1155Enumerable.sol";
+
 pragma solidity ^0.8.0;
 
-interface IPoolBase {
+/**
+ * @notice Base Pool interface, including ERC1155 functions
+ */
+interface IPoolBase is IERC1155, IERC1155Enumerable {
     /// @notice get token collection name
     /// @return collection name
     function name() external view returns (string memory);
