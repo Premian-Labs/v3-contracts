63b5937be65f45d8e72931a98c82c1749eb97883
elrond100 <magnus@openhedge.com>
2023-01-27 18:48:15 +0100

    Adding OptionMath unit tests for normalCdf.

---
+++ contracts/test/libraries/OptionMathMock.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import {OptionMath, SD59x18} from "../../libraries/OptionMath.sol";
+
+contract OptionMathMock {
+
+    function helperNormal(SD59x18 x) external pure returns (SD59x18) {
+        return OptionMath._helperNormal(x);
+    }
+
+    function normalCdf(SD59x18 x) external pure returns (SD59x18) {
+        return OptionMath._normalCdf(x);
+    }
+
+    function blackScholesPrice(
+        SD59x18 spot59x18,
+        SD59x18 strike59x18,
+        SD59x18 timeToMaturity59x18,
+        SD59x18 varAnnualized59x18,
+        bool isCall
+    ) external pure returns (SD59x18) {
+        return OptionMath._blackScholesPrice(
+            spot59x18,
+            strike59x18,
+            timeToMaturity59x18,
+            varAnnualized59x18,
+            isCall
+        );
+    }
+
+}
