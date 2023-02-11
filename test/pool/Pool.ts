28e1caae2346b64edc5b81ec93a23b84b15528fd
Lumyo <lumyo@proton.me>
2023-02-11 22:38:55 +0100

    Replace `repeat` by `parseEther`

--- test/pool/Pool.ts
+++ test/pool/Pool.ts
@@ -77,7 +77,7 @@ describe('Pool', () => {
       deployer,
       base.address,
       baseOracle.address,
-      BigNumber.from('1' + '0'.repeat(17)), // 10%
+      parseEther('0.1'), // 10%
       true,
       true,
     );
