0e3ea21f4f63c962cce92dcf9be4d3be854f0ff5
Lumyo <lumyo@proton.me>
2023-02-23 17:02:08 +0100

    Update deployment script

--- scripts/deploy/0000-deploy.ts
+++ scripts/deploy/0000-deploy.ts
@@ -8,33 +8,52 @@ import {
 
 async function main() {
   const [deployer] = await ethers.getSigners();
+  const chainId = await deployer.getChainId();
 
   //////////////////////////
 
-  // Arbitrum addresses
-  const weth = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1';
-  const wbtc = '0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f';
+  let weth: string;
+  let wbtc: string;
+  let feeReceiver: string;
+  let chainlinkAdapter: string;
 
+  if (chainId === 42161) {
+    // Arbitrum addresses
+    weth = '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1';
+    wbtc = '0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f';
+    chainlinkAdapter = '';
+    feeReceiver = '';
+  } else if (chainId === 5) {
+    // Goerli addresses
+    weth = '';
+    wbtc = '';
+    chainlinkAdapter = '';
+    feeReceiver = '';
+  } else {
+    throw new Error('ChainId not implemented');
+  }
   //////////////////////////
   // Deploy ChainlinkAdapter
 
-  const chainlinkAdapterImpl = await new ChainlinkAdapter__factory(
-    deployer,
-  ).deploy(weth, wbtc);
-  await chainlinkAdapterImpl.deployed();
-  console.log(`ChainlinkAdapter impl : ${chainlinkAdapterImpl.address}`);
+  if (chainlinkAdapter === '') {
+    const chainlinkAdapterImpl = await new ChainlinkAdapter__factory(
+      deployer,
+    ).deploy(weth, wbtc);
+    await chainlinkAdapterImpl.deployed();
+    console.log(`ChainlinkAdapter impl : ${chainlinkAdapterImpl.address}`);
 
-  const chainlinkAdapterProxy = await new ChainlinkAdapterProxy__factory(
-    deployer,
-  ).deploy(chainlinkAdapterImpl.address);
-  await chainlinkAdapterProxy.deployed();
+    const chainlinkAdapterProxy = await new ChainlinkAdapterProxy__factory(
+      deployer,
+    ).deploy(chainlinkAdapterImpl.address);
+    await chainlinkAdapterProxy.deployed();
 
-  console.log(`ChainlinkAdapter proxy : ${chainlinkAdapterProxy.address}`);
+    console.log(`ChainlinkAdapter proxy : ${chainlinkAdapterProxy.address}`);
+
+    chainlinkAdapter = chainlinkAdapterProxy.address;
+  }
 
   //////////////////////////
 
-  const chainlinkAdapter = chainlinkAdapterProxy.address;
-  const feeReceiver = ''; // ToDo : Set
   const discountPerPool = parseEther('0.1'); // 10%
   const log = true;
   const isDevMode = false;
