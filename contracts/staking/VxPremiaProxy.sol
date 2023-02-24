626d6bd3253ed53ad6e1a9e256933e33928feb48
Lumyo <lumyo@proton.me>
2023-02-24 14:22:36 +0100

    Add VxPremia contracts from v2 repo

---
+++ contracts/staking/VxPremiaProxy.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BUSL-1.1
+// For further clarification please see https://license.premia.legal
+
+pragma solidity ^0.8.0;
+
+import {ERC20MetadataInternal} from "@solidstate/contracts/token/ERC20/metadata/ERC20MetadataInternal.sol";
+
+import {ProxyUpgradeableOwnable} from "../proxy/ProxyUpgradeableOwnable.sol";
+
+contract VxPremiaProxy is ProxyUpgradeableOwnable, ERC20MetadataInternal {
+    constructor(
+        address implementation
+    ) ProxyUpgradeableOwnable(implementation) {
+        _setName("vxPremia");
+        _setSymbol("vxPREMIA");
+        _setDecimals(18);
+    }
+}
