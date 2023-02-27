4ac01c20692ed9b9379e1274215bef7cefaabb4b
elrond100 <magnus@openhedge.com>
2023-02-27 11:30:32 +0100

    Return fixed value for _getSpotPrice. Needs fixing / mocking.

--- contracts/vaults/underwriter/UnderwriterVault.sol
+++ contracts/vaults/underwriter/UnderwriterVault.sol
@@ -88,10 +88,14 @@ contract UnderwriterVault is
     ) internal view returns (uint256) {
         UnderwriterVaultStorage.Layout storage l = UnderwriterVaultStorage
             .layout();
-        uint256 price = IOracleAdapter(oracleAdapterAddr).quote(
+
+        // TODO
+        /* uint256 price = IOracleAdapter(oracleAdapterAddr).quote(
             l.base,
             l.quote
         );
+        */
+        uint256 price = 1000000000000000000;
         return price;
     }
 
