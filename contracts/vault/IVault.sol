069d775ea3d6abdfe69b2bb443aa2937df634ddf
Froggie <froggie@premia.finance>
2023-03-18 19:40:19 -1000

    Add necessary vault events for subgraph

--- contracts/vault/IVault.sol
+++ contracts/vault/IVault.sol
@@ -5,6 +5,36 @@ pragma solidity ^0.8.0;
 interface IVault {
     event UpdateQuotes();
 
+    event Deposit(
+        address indexed sender,
+        address indexed owner,
+        uint256 assets,
+        uint256 shares
+    );
+
+    event Withdraw(
+        address indexed sender,
+        address indexed receiver,
+        address indexed owner,
+        uint256 assets,
+        uint256 shares
+    );
+
+    event Trade(
+        address indexed user,
+        address indexed pool,
+        uint256 size,
+        bool isBuy,
+        uint256 premium,
+        uint256 takerFeePaid,
+        uint256 makerRebateReceived,
+        uint256 protocolFee
+    );
+
+    event ManagementFeePaid(address indexed receiver, uint256 managementFee);
+
+    event PerformanceFeePaid(address indexed receiver, uint256 performanceFee);
+
     function getTradeQuote(
         uint256 strike,
         uint64 maturity,
