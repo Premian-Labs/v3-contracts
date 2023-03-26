4d8152f7abaf0bdd5ee95641c4f6e84b2b62545d
Lumyo <lumyo@proton.me>
2023-03-26 01:23:27 +0100

    PoolFactory tests

--- test/foundry/PoolFactory.t.sol
+++ test/foundry/PoolFactory.t.sol
@@ -19,7 +19,7 @@ contract PoolFactoryTest is Test {
     PoolFactory factory;
     Premia diamond;
 
-    fallback() external payable {}
+    receive() external payable {}
 
     function setUp() public {
         vm.warp(1679758940);
@@ -58,25 +58,123 @@ contract PoolFactoryTest is Test {
         factory = PoolFactory(address(proxy));
     }
 
-    function test_PoolAddress0IfNotDeployed() public {
+    function test_getPoolAddress_ReturnAddress0IfNotDeployed() public {
         assertEq(factory.getPoolAddress(poolKey), address(0));
     }
 
-    function test_RevertIfBaseAndQuoteEqual() public {
+    function test_getPoolAddress_ReturnPoolAddressIfDeployed() public {
+        address poolAddress = factory.deployPool{value: 1 ether}(poolKey);
+
+        assertEq(factory.getPoolAddress(poolKey), poolAddress);
+    }
+
+    function test_deployPool_DeployPool() public {
+        factory.deployPool{value: 1 ether}(poolKey);
+    }
+
+    function test_deployPool_RevertIf_BaseAndQuoteEqual() public {
         vm.expectRevert(IPoolFactory.PoolFactory__IdenticalAddresses.selector);
 
         poolKey.base = poolKey.quote;
         factory.deployPool{value: 1 ether}(poolKey);
     }
 
-    function test_deployPool() public {
+    function test_deployPool_RevertIf_QuoteZeroAddress() public {
+        poolKey.quote = address(0);
+
+        vm.expectRevert(IPoolFactory.PoolFactory__ZeroAddress.selector);
         factory.deployPool{value: 1 ether}(poolKey);
     }
 
-    function test_RevertIfDeployed() public {
+    function test_deployPool_RevertIf_BaseZeroAddress() public {
+        poolKey.base = address(0);
+
+        vm.expectRevert(IPoolFactory.PoolFactory__ZeroAddress.selector);
+        factory.deployPool{value: 1 ether}(poolKey);
+    }
+
+    function test_deployPool_RevertIf_OracleAddress() public {
+        poolKey.oracleAdapter = address(0);
+
+        vm.expectRevert(IPoolFactory.PoolFactory__ZeroAddress.selector);
+        factory.deployPool{value: 1 ether}(poolKey);
+    }
+
+    function test_deployPool_RevertIf_StrikeIsZero() public {
+        poolKey.strike = UD60x18.wrap(0);
+
+        vm.expectRevert(
+            IPoolFactory.PoolFactory__OptionStrikeEqualsZero.selector
+        );
+        factory.deployPool{value: 1 ether}(poolKey);
+    }
+
+    function test_deployPool_RevertIf_AlreadyDeployed() public {
         factory.deployPool{value: 1 ether}(poolKey);
 
         vm.expectRevert(IPoolFactory.PoolFactory__PoolAlreadyDeployed.selector);
+        factory.deployPool{value: 1 ether}(poolKey);
+    }
+
+    function test_deployPool_RevertIf_PriceNotWithinStrikeInterval() public {
+        uint256[4] memory strike = [
+            uint256(99990 ether),
+            uint256(1050 ether),
+            uint256(950 ether),
+            uint256(11 ether)
+        ];
+
+        for (uint256 i; i < strike.length; i++) {
+            poolKey.strike = UD60x18.wrap(strike[i]);
+
+            vm.expectRevert(
+                IPoolFactory.PoolFactory__OptionStrikeInvalid.selector
+            );
+
+            factory.deployPool{value: 1 ether}(poolKey);
+        }
+    }
+
+    function test_deployPool_RevertIf_MaturityExpired() public {
+        poolKey.maturity = 1679758930;
+
+        vm.expectRevert(IPoolFactory.PoolFactory__OptionExpired.selector);
+        factory.deployPool{value: 1 ether}(poolKey);
+    }
+
+    function test_deployPool_RevertIf_MaturityNot8UTC() public {
+        poolKey.maturity = 1679768950;
+
+        vm.expectRevert(
+            IPoolFactory.PoolFactory__OptionMaturityNot8UTC.selector
+        );
+        factory.deployPool{value: 1 ether}(poolKey);
+    }
+
+    function test_deployPool_RevertIf_MaturityWeeklyNotFriday() public {
+        poolKey.maturity = 1680163200;
+
+        vm.expectRevert(
+            IPoolFactory.PoolFactory__OptionMaturityNotFriday.selector
+        );
+        factory.deployPool{value: 1 ether}(poolKey);
+    }
+
+    function test_deployPool_RevertIf_MaturityMonthlyNotLastFriday() public {
+        poolKey.maturity = 1683878400;
+
+        vm.expectRevert(
+            IPoolFactory.PoolFactory__OptionMaturityNotLastFriday.selector
+        );
+        factory.deployPool{value: 1 ether}(poolKey);
+    }
+
+    function test_deployPool_RevertIf_MaturityExceedsOneYear() public {
+        poolKey.maturity = 1714118400;
+
+        vm.expectRevert(
+            IPoolFactory.PoolFactory__OptionMaturityExceedsMax.selector
+        );
         factory.deployPool{value: 1 ether}(poolKey);
     }
 }
