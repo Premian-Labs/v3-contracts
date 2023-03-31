bbb5acc51d31e5c1343ed4bcfe4ac5c066ed77a9
Lumyo <lumyo@proton.me>
2023-03-31 22:38:03 +0200

    Fix hardhat tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import '@nomicfoundation/hardhat-chai-matchers';
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-etherscan';
 import '@solidstate/hardhat-4byte-uploader';
+import '@solidstate/hardhat-test-short-circuit';
 import '@typechain/hardhat';
 import Dotenv from 'dotenv';
 import 'hardhat-abi-exporter';
