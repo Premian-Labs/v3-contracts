8ed915b9bb5c06189ae3582dbfcb82d2e559cb7c
Lumyo <lumyo@proton.me>
2023-04-19 15:34:31 +0200

    Add tests

---
+++ contracts/test/pool/FlashLoanMock.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {IERC20} from "@solidstate/contracts/interfaces/IERC20.sol";
+
+import {IFlashLoanCallback} from "../../pool/IFlashLoanCallback.sol";
+import {IPool} from "../../pool/IPool.sol";
+
+contract FlashLoanMock is IFlashLoanCallback {
+    struct FlashLoan {
+        address pool;
+        uint256 amount;
+    }
+
+    function singleFlashLoan(FlashLoan memory loan, bool repayFull) external {
+        IPool(loan.pool).flashLoan(
+            loan.amount,
+            abi.encode(new FlashLoan[](0), repayFull)
+        );
+    }
+
+    function multiFlashLoan(FlashLoan[] memory loans) external {
+        FlashLoan memory loan = loans[loans.length - 1];
+
+        // Remove last element from array
+        assembly {
+            mstore(loans, sub(mload(loans), 1))
+        }
+
+        IPool(loan.pool).flashLoan(loan.amount, abi.encode(loans, true));
+    }
+
+    function premiaFlashLoanCallback(
+        address token,
+        uint256 amountToRepay,
+        bytes memory data
+    ) external {
+        (FlashLoan[] memory loans, bool repayFull) = abi.decode(
+            data,
+            (FlashLoan[], bool)
+        );
+
+        if (loans.length > 0) {
+            FlashLoan memory nextLoan = loans[loans.length - 1];
+
+            // Remove last element from array
+            assembly {
+                mstore(loans, sub(mload(loans), 1))
+            }
+
+            IPool(nextLoan.pool).flashLoan(
+                nextLoan.amount,
+                abi.encode(loans, true)
+            );
+        }
+
+        IERC20(token).transfer(
+            msg.sender,
+            repayFull ? amountToRepay : amountToRepay - 1
+        );
+    }
+}
--- test-foundry/pool/_Pool.t.sol
+++ test-foundry/pool/_Pool.t.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.19;
 
 import {PoolDepositTest} from "./Pool.deposit.t.sol";
 import {PoolFillQuoteRFQTest} from "./Pool.fillQuoteRFQ.t.sol";
+import {PoolFlashLoanTest} from "./Pool.flashLoan.t.sol";
 import {PoolTradeTest} from "./Pool.trade.t.sol";
 import {PoolWithdrawTest} from "./Pool.withdraw.t.sol";
 import {PoolStrandedTest} from "./Pool.stranded.t.sol";
@@ -11,6 +12,7 @@ import {PoolStrandedTest} from "./Pool.stranded.t.sol";
 abstract contract PoolTest is
     PoolDepositTest,
     PoolFillQuoteRFQTest,
+    PoolFlashLoanTest,
     PoolStrandedTest,
     PoolTradeTest,
     PoolWithdrawTest
