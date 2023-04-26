78dffd7ddf60c972a8e0ff5c0e51ac91589bf06d
Lumyo <lumyo@proton.me>
2023-04-27 00:03:37 +0200

    Update test-foundry/pool/Pool.flashLoan.t.sol
    Co-authored-by: 0xCourtney <23440493+0xCourtney@users.noreply.github.com>

--- test-foundry/pool/Pool.flashLoan.t.sol
+++ test-foundry/pool/Pool.flashLoan.t.sol
@@ -2,7 +2,6 @@
 
 pragma solidity >=0.8.19;
 
-import "forge-std/console2.sol";
 import {UD60x18} from "@prb/math/UD60x18.sol";
 
 import {IERC20} from "@solidstate/contracts/interfaces/IERC20.sol";
