21bd935b9263f3c0d591b9403d45ab8267bde496
Lumyo <lumyo@proton.me>
2023-05-12 16:04:12 +0200

    Trade tests

--- test-foundry/pool/Pool.trade.t.sol
+++ test-foundry/pool/Pool.trade.t.sol
@@ -76,6 +76,143 @@ abstract contract PoolTradeTest is DeployTest {
         _test_trade_Sell50Options_WithApproval(poolKey.isCallPool);
     }
 
+    function _test_trade_RevertIf_BuyOptions_WithTotalPremiumAboveLimit(
+        bool isCall
+    ) internal {
+        posKey.orderType = Position.OrderType.CS;
+        deposit(1000 ether);
+
+        UD60x18 tradeSize = ud(500 ether);
+        (uint256 totalPremium, ) = pool.getQuoteAMM(
+            users.trader,
+            tradeSize,
+            true
+        );
+
+        address poolToken = getPoolToken(isCall);
+
+        vm.startPrank(users.trader);
+        deal(poolToken, users.trader, totalPremium);
+        IERC20(poolToken).approve(address(router), totalPremium);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IPoolInternal.Pool__AboveMaxSlippage.selector,
+                totalPremium - 1,
+                0,
+                totalPremium
+            )
+        );
+        pool.trade(tradeSize, true, totalPremium - 1);
+    }
+
+    function test_trade_RevertIf_BuyOptions_WithTotalPremiumAboveLimit()
+        public
+    {
+        _test_trade_RevertIf_BuyOptions_WithTotalPremiumAboveLimit(
+            poolKey.isCallPool
+        );
+    }
+
+    function _test_trade_RevertIf_SellOptions_WithTotalPremiumBelowLimit(
+        bool isCall
+    ) internal {
+        deposit(1000 ether);
+
+        UD60x18 tradeSize = ud(500 ether);
+        uint256 collateralScaled = scaleDecimals(
+            contractsToCollateral(tradeSize, isCall),
+            isCall
+        );
+
+        (uint256 totalPremium, ) = pool.getQuoteAMM(
+            users.trader,
+            tradeSize,
+            false
+        );
+
+        address poolToken = getPoolToken(isCall);
+
+        vm.startPrank(users.trader);
+        deal(poolToken, users.trader, collateralScaled);
+        IERC20(poolToken).approve(address(router), collateralScaled);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IPoolInternal.Pool__AboveMaxSlippage.selector,
+                totalPremium + 1,
+                totalPremium,
+                type(uint256).max
+            )
+        );
+        pool.trade(tradeSize, false, totalPremium + 1);
+    }
+
+    function test_trade_RevertIf_SellOptions_WithTotalPremiumBelowLimit()
+        public
+    {
+        _test_trade_RevertIf_SellOptions_WithTotalPremiumBelowLimit(
+            poolKey.isCallPool
+        );
+    }
+
+    function _test_trade_RevertIf_BuyOptions_WithInsufficientAskLiquidity(
+        bool isCall
+    ) internal {
+        posKey.orderType = Position.OrderType.CS;
+        uint256 depositSize = 1000 ether;
+        deposit(depositSize);
+
+        vm.expectRevert(IPoolInternal.Pool__InsufficientAskLiquidity.selector);
+        pool.trade(ud(depositSize + 1), true, 0);
+    }
+
+    function test_trade_RevertIf_BuyOptions_WithInsufficientAskLiquidity()
+        public
+    {
+        _test_trade_RevertIf_BuyOptions_WithInsufficientAskLiquidity(
+            poolKey.isCallPool
+        );
+    }
+
+    function _test_trade_RevertIf_SellOptions_WithInsufficientBidLiquidity(
+        bool isCall
+    ) internal {
+        uint256 depositSize = 1000 ether;
+        deposit(depositSize);
+
+        vm.expectRevert(IPoolInternal.Pool__InsufficientBidLiquidity.selector);
+        pool.trade(ud(depositSize + 1), false, 0);
+    }
+
+    function test_trade_RevertIf_SellOptions_WithInsufficientBidLiquidity()
+        public
+    {
+        _test_trade_RevertIf_SellOptions_WithInsufficientBidLiquidity(
+            poolKey.isCallPool
+        );
+    }
+
+    function _test_trade_RevertIf_TradeSizeIsZero(bool isCall) internal {
+        vm.expectRevert(IPoolInternal.Pool__ZeroSize.selector);
+        pool.trade(ud(0), true, 0);
+    }
+
+    function test_trade_RevertIf_TradeSizeIsZero() public {
+        _test_trade_RevertIf_TradeSizeIsZero(poolKey.isCallPool);
+    }
+
+    function _test_trade_RevertIf_Expired(bool isCall) internal {
+        vm.warp(poolKey.maturity);
+
+        vm.expectRevert(IPoolInternal.Pool__OptionExpired.selector);
+        pool.trade(ud(1), true, 0);
+    }
+
+    function test_trade_RevertIf_Expired() public {
+        _test_trade_RevertIf_Expired(poolKey.isCallPool);
+    }
+
     function _test_annihilate_Success(bool isCall) internal {
         deposit(1000 ether);
         vm.startPrank(users.lp);
