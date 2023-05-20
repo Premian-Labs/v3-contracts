f1399e714f0a5ce39110517a5f8c35912bf3ec9e
Lumyo <lumyo@proton.me>
2023-05-20 17:35:20 +0200

    Vault internal tests

--- test-foundry/vault/strategies/underwriter/_UnderwriterVault.t.sol
+++ test-foundry/vault/strategies/underwriter/_UnderwriterVault.t.sol
@@ -4,12 +4,18 @@ pragma solidity >=0.8.19;
 
 import {UnderwriterVaultErc4626Test} from "./UnderwriterVault.erc4626.t.sol";
 import {UnderwriterVaultFeesTest} from "./UnderwriterVault.fees.t.sol";
+import {UnderwriterVaultInternalTest} from "./UnderwriterVault.internal.t.sol";
 import {UnderwriterVaultPpsTest} from "./UnderwriterVault.pps.t.sol";
+import {UnderwriterVaultRevertIfTest} from "./UnderwriterVault.revertIf.t.sol";
 import {UnderwriterVaultStorageTest} from "./UnderwriterVault.storage.t.sol";
+import {UnderwriterVaultVaultTest} from "./UnderwriterVault.vault.t.sol";
 
 abstract contract UnderwriterVaultTest is
     UnderwriterVaultErc4626Test,
     UnderwriterVaultFeesTest,
+    UnderwriterVaultInternalTest,
     UnderwriterVaultPpsTest,
-    UnderwriterVaultStorageTest
+    UnderwriterVaultRevertIfTest,
+    UnderwriterVaultStorageTest,
+    UnderwriterVaultVaultTest
 {}
