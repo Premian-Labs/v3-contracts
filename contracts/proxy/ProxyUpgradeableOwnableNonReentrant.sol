992eaf3c0b568f68bb6f9175c2011e1db9ed9e9e
0xCourtney <23440493+0xCourtney@users.noreply.github.com>
2023-06-29 10:34:30 -0400

    unused import

--- contracts/proxy/ProxyUpgradeableOwnableNonReentrant.sol
+++ contracts/proxy/ProxyUpgradeableOwnableNonReentrant.sol
@@ -2,7 +2,6 @@
 
 pragma solidity >=0.8.19;
 
-import {AddressUtils} from "@solidstate/contracts/utils/AddressUtils.sol";
 import {SafeOwnable} from "@solidstate/contracts/access/ownable/SafeOwnable.sol";
 import {OwnableInternal} from "@solidstate/contracts/access/ownable/OwnableInternal.sol";
 
@@ -11,8 +10,6 @@ import {ProxyUpgradeableOwnable} from "./ProxyUpgradeableOwnable.sol";
 import {ReentrancyGuardExtended} from "../utils/ReentrancyGuardExtended.sol";
 
 contract ProxyUpgradeableOwnableNonReentrant is ProxyUpgradeableOwnable, ReentrancyGuardExtended {
-    using AddressUtils for address;
-
     constructor(address implementation) ProxyUpgradeableOwnable(implementation) {}
 
     function _handleDelegateCalls() internal override nonReentrant returns (bool result, bytes memory data) {
