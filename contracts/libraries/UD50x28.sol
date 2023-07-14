09b8b9f1dae8c134faa94f1a90b828d17b14567f
Lumyo <lumyo@proton.me>
2023-07-14 12:26:56 +0200

    Finish to implement UD50x28

--- contracts/libraries/UD50x28.sol
+++ contracts/libraries/UD50x28.sol
@@ -1,26 +1,222 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity >=0.8.19;
+pragma solidity ^0.8.19;
 
+import {mulDiv} from "@prb/math/Common.sol";
 import {UD60x18} from "@prb/math/UD60x18.sol";
 
 type UD50x28 is uint256;
 
-UD60x18 constant SCALING_FACTOR = UD60x18.wrap(1e28);
+uint256 constant SCALING_FACTOR = 1e28;
 uint256 constant uMAX_UD50x28 = 11579208923731619542357098500868790785326998466564_0564039457584007913129639935;
 
+/// @dev The unit number, which gives the decimal precision of UD50x28.
+uint256 constant uUNIT = 1e28;
+UD50x28 constant UNIT = UD50x28.wrap(uUNIT);
+
 error UD50x28_IntoUD50x28_Overflow(UD60x18 x);
+
+/// @notice Wraps a uint256 number into the UD60x18 value type.
+function wrap(uint256 x) pure returns (UD50x28 result) {
+    result = UD50x28.wrap(x);
+}
+
+/// @notice Unwraps a UD60x18 number into uint256.
+function unwrap(UD50x28 x) pure returns (uint256 result) {
+    result = UD50x28.unwrap(x);
+}
 
 function ud50x28(uint256 x) pure returns (UD50x28 result) {
     result = UD50x28.wrap(x);
 }
 
-function intoUD50x28(UD60x18 x) pure returns (UD50x28 result) {
-    uint256 xUint = UD60x18.unwrap(x / SCALING_FACTOR);
+function ud50x28(UD60x18 x) pure returns (UD50x28 result) {
+    uint256 xUint = x.unwrap() * SCALING_FACTOR;
     if (xUint > uMAX_UD50x28) revert UD50x28_IntoUD50x28_Overflow(x);
-    result = UD50x28.wrap(xUint);
+    result = wrap(xUint);
 }
 
 function intoUD60x18(UD50x28 x) pure returns (UD60x18 result) {
-    result = UD60x18.wrap(UD50x28.unwrap(x)) * SCALING_FACTOR;
+    result = UD60x18.wrap(x.unwrap() / SCALING_FACTOR);
 }
+
+/// @notice Implements the checked addition operation (+) in the UD50x28 type.
+function add(UD50x28 x, UD50x28 y) pure returns (UD50x28 result) {
+    result = wrap(x.unwrap() + y.unwrap());
+}
+
+/// @notice Implements the AND (&) bitwise operation in the UD50x28 type.
+function and(UD50x28 x, uint256 bits) pure returns (UD50x28 result) {
+    result = wrap(x.unwrap() & bits);
+}
+
+/// @notice Implements the AND (&) bitwise operation in the UD50x28 type.
+function and2(UD50x28 x, UD50x28 y) pure returns (UD50x28 result) {
+    result = wrap(x.unwrap() & y.unwrap());
+}
+
+/// @notice Implements the equal operation (==) in the UD50x28 type.
+function eq(UD50x28 x, UD50x28 y) pure returns (bool result) {
+    result = x.unwrap() == y.unwrap();
+}
+
+/// @notice Implements the greater than operation (>) in the UD50x28 type.
+function gt(UD50x28 x, UD50x28 y) pure returns (bool result) {
+    result = x.unwrap() > y.unwrap();
+}
+
+/// @notice Implements the greater than or equal to operation (>=) in the UD50x28 type.
+function gte(UD50x28 x, UD50x28 y) pure returns (bool result) {
+    result = x.unwrap() >= y.unwrap();
+}
+
+/// @notice Implements a zero comparison check function in the UD50x28 type.
+function isZero(UD50x28 x) pure returns (bool result) {
+    // This wouldn't work if x could be negative.
+    result = x.unwrap() == 0;
+}
+
+/// @notice Implements the left shift operation (<<) in the UD50x28 type.
+function lshift(UD50x28 x, uint256 bits) pure returns (UD50x28 result) {
+    result = wrap(x.unwrap() << bits);
+}
+
+/// @notice Implements the lower than operation (<) in the UD50x28 type.
+function lt(UD50x28 x, UD50x28 y) pure returns (bool result) {
+    result = x.unwrap() < y.unwrap();
+}
+
+/// @notice Implements the lower than or equal to operation (<=) in the UD50x28 type.
+function lte(UD50x28 x, UD50x28 y) pure returns (bool result) {
+    result = x.unwrap() <= y.unwrap();
+}
+
+/// @notice Implements the checked modulo operation (%) in the UD50x28 type.
+function mod(UD50x28 x, UD50x28 y) pure returns (UD50x28 result) {
+    result = wrap(x.unwrap() % y.unwrap());
+}
+
+/// @notice Implements the not equal operation (!=) in the UD50x28 type.
+function neq(UD50x28 x, UD50x28 y) pure returns (bool result) {
+    result = x.unwrap() != y.unwrap();
+}
+
+/// @notice Implements the NOT (~) bitwise operation in the UD50x28 type.
+function not(UD50x28 x) pure returns (UD50x28 result) {
+    result = wrap(~x.unwrap());
+}
+
+/// @notice Implements the OR (|) bitwise operation in the UD50x28 type.
+function or(UD50x28 x, UD50x28 y) pure returns (UD50x28 result) {
+    result = wrap(x.unwrap() | y.unwrap());
+}
+
+/// @notice Implements the right shift operation (>>) in the UD50x28 type.
+function rshift(UD50x28 x, uint256 bits) pure returns (UD50x28 result) {
+    result = wrap(x.unwrap() >> bits);
+}
+
+/// @notice Implements the checked subtraction operation (-) in the UD50x28 type.
+function sub(UD50x28 x, UD50x28 y) pure returns (UD50x28 result) {
+    result = wrap(x.unwrap() - y.unwrap());
+}
+
+/// @notice Implements the unchecked addition operation (+) in the UD50x28 type.
+function uncheckedAdd(UD50x28 x, UD50x28 y) pure returns (UD50x28 result) {
+    unchecked {
+        result = wrap(x.unwrap() + y.unwrap());
+    }
+}
+
+/// @notice Implements the unchecked subtraction operation (-) in the UD50x28 type.
+function uncheckedSub(UD50x28 x, UD50x28 y) pure returns (UD50x28 result) {
+    unchecked {
+        result = wrap(x.unwrap() - y.unwrap());
+    }
+}
+
+/// @notice Implements the XOR (^) bitwise operation in the UD50x28 type.
+function xor(UD50x28 x, UD50x28 y) pure returns (UD50x28 result) {
+    result = wrap(x.unwrap() ^ y.unwrap());
+}
+
+/// @notice Divides two UD50x28 numbers, returning a new UD50x28 number.
+///
+/// @dev Uses {Common.mulDiv} to enable overflow-safe multiplication and division.
+///
+/// Notes:
+/// - Refer to the notes in {Common.mulDiv}.
+///
+/// Requirements:
+/// - Refer to the requirements in {Common.mulDiv}.
+///
+/// @param x The numerator as a UD50x28 number.
+/// @param y The denominator as a UD50x28 number.
+/// @param result The quotient as a UD50x28 number.
+/// @custom:smtchecker abstract-function-nondet
+function div(UD50x28 x, UD50x28 y) pure returns (UD50x28 result) {
+    result = UD50x28.wrap(mulDiv(x.unwrap(), uUNIT, y.unwrap()));
+}
+
+/// @notice Multiplies two UD60x18 numbers together, returning a new UD60x18 number.
+///
+/// @dev Uses {Common.mulDiv} to enable overflow-safe multiplication and division.
+///
+/// Notes:
+/// - Refer to the notes in {Common.mulDiv}.
+///
+/// Requirements:
+/// - Refer to the requirements in {Common.mulDiv}.
+///
+/// @dev See the documentation in {Common.mulDiv18}.
+/// @param x The multiplicand as a UD50x28 number.
+/// @param y The multiplier as a UD50x28 number.
+/// @return result The product as a UD50x28 number.
+/// @custom:smtchecker abstract-function-nondet
+function mul(UD50x28 x, UD50x28 y) pure returns (UD50x28 result) {
+    result = UD50x28.wrap(mulDiv(x.unwrap(), y.unwrap(), uUNIT));
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+// The global "using for" directive makes the functions in this library callable on the UD50x28 type.
+using {
+    unwrap,
+    add,
+    and,
+    eq,
+    gt,
+    gte,
+    isZero,
+    lshift,
+    lt,
+    lte,
+    mod,
+    neq,
+    not,
+    or,
+    rshift,
+    sub,
+    uncheckedAdd,
+    uncheckedSub,
+    xor
+} for UD50x28 global;
+
+// The global "using for" directive makes it possible to use these operators on the UD50x28 type.
+using {
+    add as +,
+    and2 as &,
+    div as /,
+    eq as ==,
+    gt as >,
+    gte as >=,
+    lt as <,
+    lte as <=,
+    or as |,
+    mod as %,
+    mul as *,
+    neq as !=,
+    not as ~,
+    sub as -,
+    xor as ^
+} for UD50x28 global;
