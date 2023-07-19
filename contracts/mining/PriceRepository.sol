4eeb7bea1eca5acc6e50216350a6ccd92029ccae
Lumyo <lumyo.b@gmail.com>
2023-07-19 11:31:44 +0200

    Update `pragma solidity` + licenses

--- contracts/mining/PriceRepository.sol
+++ contracts/mining/PriceRepository.sol
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: UNLICENSED
-
-pragma solidity >=0.8.19;
+// SPDX-License-Identifier: LicenseRef-P3-DUAL
+// For terms and conditions regarding commercial use please see https://license.premia.blue
+pragma solidity ^0.8.19;
 
 import {UD60x18, ud} from "@prb/math/UD60x18.sol";
 import {EnumerableSet} from "@solidstate/contracts/data/EnumerableSet.sol";
