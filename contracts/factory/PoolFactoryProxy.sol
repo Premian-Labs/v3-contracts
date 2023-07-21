f1bb364ad714743cf06df132bf74272562625c2f
Lumyo <lumyo.b@gmail.com>
2023-07-21 17:09:58 +0200

    Add `discountPerPool` validation in `PoolFactoryProxy`

--- contracts/factory/PoolFactoryProxy.sol
+++ contracts/factory/PoolFactoryProxy.sol
@@ -4,11 +4,13 @@ pragma solidity >=0.8.19;
 
 import {UD60x18} from "@prb/math/UD60x18.sol";
 
+import {IPoolFactory} from "./IPoolFactory.sol";
 import {IPoolFactoryEvents} from "./IPoolFactoryEvents.sol";
 
 import {PoolFactoryStorage} from "./PoolFactoryStorage.sol";
 
 import {ProxyUpgradeableOwnable} from "../proxy/ProxyUpgradeableOwnable.sol";
+import {ZERO, ONE} from "../libraries/Constants.sol";
 
 contract PoolFactoryProxy is IPoolFactoryEvents, ProxyUpgradeableOwnable {
     using PoolFactoryStorage for PoolFactoryStorage.Layout;
@@ -20,6 +22,7 @@ contract PoolFactoryProxy is IPoolFactoryEvents, ProxyUpgradeableOwnable {
     ) ProxyUpgradeableOwnable(implementation) {
         PoolFactoryStorage.Layout storage l = PoolFactoryStorage.layout();
 
+        if (discountPerPool == ZERO || discountPerPool >= ONE) revert IPoolFactory.PoolFactory__InvalidInput();
         l.discountPerPool = discountPerPool;
         emit SetDiscountPerPool(discountPerPool);
 
