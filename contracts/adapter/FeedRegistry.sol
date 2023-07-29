3d3324be1a4ed2c864ee89b51a8e5d6e348174bc
Lumyo <lumyo@proton.me>
2023-07-29 10:49:53 +0200

    Fixed solidity version on contracts

--- contracts/adapter/FeedRegistry.sol
+++ contracts/adapter/FeedRegistry.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: LicenseRef-P3-DUAL
 // For terms and conditions regarding commercial use please see https://license.premia.blue
-pragma solidity ^0.8.19;
+pragma solidity =0.8.19;
 
 import {Denominations} from "@chainlink/contracts/src/v0.8/Denominations.sol";
 
