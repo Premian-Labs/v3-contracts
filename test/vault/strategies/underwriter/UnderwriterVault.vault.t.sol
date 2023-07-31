ec0949ce84d3f2f6d482351b6180742d07aebe6f
wolfy <96598779+wolfyds@users.noreply.github.com>
2023-07-31 11:31:27 -0400

    Update test/vault/strategies/underwriter/UnderwriterVault.vault.t.sol
    Co-authored-by: Lumyo <lumyo@proton.me>

--- test/vault/strategies/underwriter/UnderwriterVault.vault.t.sol
+++ test/vault/strategies/underwriter/UnderwriterVault.vault.t.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.19;
 import "forge-std/console2.sol";
 
 import {UD60x18, ud} from "@prb/math/UD60x18.sol";
-import {SD59x18, sd} from "@prb/math/SD59x18.sol";
 
 import {IERC20} from "@solidstate/contracts/interfaces/IERC20.sol";
 
