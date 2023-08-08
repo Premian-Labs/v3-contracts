cd31f4c2c0a0e030486e34325e500f9562e2de94
Lumyo <lumyo@proton.me>
2023-08-09 01:38:18 +0200

    Update json deployment file structure

--- utils/deployment/types.ts
+++ utils/deployment/types.ts
@@ -5,37 +5,55 @@ export interface ContractAddresses {
   optionReward: { [name: string]: string };
   feeReceiver: string;
 
-  ChainlinkAdapterImplementation: string;
-  ChainlinkAdapterProxy: string;
-  PremiaDiamond: string;
-  PoolFactoryImplementation: string;
-  PoolFactoryProxy: string;
-  PoolFactoryDeployer: string;
-  UserSettingsImplementation: string;
-  UserSettingsProxy: string;
-  ExchangeHelper: string;
-  ReferralImplementation: string;
-  ReferralProxy: string;
-  VxPremiaImplementation: string;
-  VxPremiaProxy: string;
-  ERC20Router: string;
-  PoolBase: string;
-  PoolCore: string;
-  PoolTrade: string;
-  OrderbookStream: string;
-  VaultRegistryImplementation: string;
-  VaultRegistryProxy: string;
-  VolatilityOracleImplementation: string;
-  VolatilityOracleProxy: string;
-  UnderwriterVaultImplementation: string;
-  VaultMiningImplementation: string;
-  VaultMiningProxy: string;
-  OptionPSFactoryImplementation: string;
-  OptionPSFactoryProxy: string;
-  OptionPSImplementation: string;
-  OptionRewardFactoryImplementation: string;
-  OptionRewardFactoryProxy: string;
-  OptionRewardImplementation: string;
+  ChainlinkAdapterImplementation: ContractDeploymentInfos;
+  ChainlinkAdapterProxy: ContractDeploymentInfos;
+  PremiaDiamond: ContractDeploymentInfos;
+  PoolFactoryImplementation: ContractDeploymentInfos;
+  PoolFactoryProxy: ContractDeploymentInfos;
+  PoolFactoryDeployer: ContractDeploymentInfos;
+  UserSettingsImplementation: ContractDeploymentInfos;
+  UserSettingsProxy: ContractDeploymentInfos;
+  ExchangeHelper: ContractDeploymentInfos;
+  ReferralImplementation: ContractDeploymentInfos;
+  ReferralProxy: ContractDeploymentInfos;
+  VxPremiaImplementation: ContractDeploymentInfos;
+  VxPremiaProxy: ContractDeploymentInfos;
+  ERC20Router: ContractDeploymentInfos;
+  PoolBase: ContractDeploymentInfos;
+  PoolCore: ContractDeploymentInfos;
+  PoolTrade: ContractDeploymentInfos;
+  OrderbookStream: ContractDeploymentInfos;
+  VaultRegistryImplementation: ContractDeploymentInfos;
+  VaultRegistryProxy: ContractDeploymentInfos;
+  VolatilityOracleImplementation: ContractDeploymentInfos;
+  VolatilityOracleProxy: ContractDeploymentInfos;
+  UnderwriterVaultImplementation: ContractDeploymentInfos;
+  VaultMiningImplementation: ContractDeploymentInfos;
+  VaultMiningProxy: ContractDeploymentInfos;
+  OptionPSFactoryImplementation: ContractDeploymentInfos;
+  OptionPSFactoryProxy: ContractDeploymentInfos;
+  OptionPSImplementation: ContractDeploymentInfos;
+  OptionRewardFactoryImplementation: ContractDeploymentInfos;
+  OptionRewardFactoryProxy: ContractDeploymentInfos;
+  OptionRewardImplementation: ContractDeploymentInfos;
+}
+
+export interface ContractDeploymentInfos {
+  address: string;
+  contractType: ContractType;
+  deploymentArgs: string[];
+  commitHash: string;
+  txHash: string;
+  block: number;
+  timestamp: number;
+}
+
+export enum ContractType {
+  Standalone = 'Standalone',
+  Proxy = 'Proxy',
+  Implementation = 'Implementation',
+  DiamondProxy = 'DiamondProxy',
+  DiamondFacet = 'DiamondFacet',
 }
 
 export enum ChainID {
