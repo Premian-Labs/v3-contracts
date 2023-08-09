db3bd3ae155749e0f349878da9efe3b9e7e5520e
Lumyo <lumyo@proton.me>
2023-08-10 00:01:46 +0200

    Optimize `safeTransferIgnoreDust`

--- contracts/pool/PoolStorage.sol
+++ contracts/pool/PoolStorage.sol
@@ -195,19 +195,21 @@ library PoolStorage {
     function safeTransferIgnoreDust(IERC20 token, address to, uint256 value) internal {
         PoolStorage.Layout storage l = PoolStorage.layout();
         uint256 balance = IERC20(l.getPoolToken()).balanceOf(address(this));
-        SD59x18 balanceSD59x18 = l.fromPoolTokenDecimals(balance).intoSD59x18();
-        SD59x18 valueSD59x18 = l.fromPoolTokenDecimals(value).intoSD59x18();
-        if (value > 0) {
-            SD59x18 relativeDiff = (valueSD59x18 - balanceSD59x18) / valueSD59x18;
-            SD59x18 relativeTolerance = sd(0.0001 ether);
-            if (relativeDiff > relativeTolerance) {
-                revert IPoolInternal.Pool__InsufficientFunds();
-            }
-            if (balance < value) {
-                value = balance;
-            }
-            token.safeTransfer(to, value);
+
+        if (value == 0) return;
+
+        if (balance < value) {
+            UD60x18 _balance = l.fromPoolTokenDecimals(balance);
+            UD60x18 _value = l.fromPoolTokenDecimals(value);
+            UD60x18 relativeDiff = (_value - _balance) / _value;
+
+            // If relativeDiff is larger than the 0.01% tolerance, then revert
+            if (relativeDiff > ud(0.0001 ether)) revert IPoolInternal.Pool__InsufficientFunds();
+
+            value = balance;
         }
+
+        token.safeTransfer(to, value);
     }
 
     /// @notice Transfers token amount to recipient. Ignores if dust is missing on the exchange level,
