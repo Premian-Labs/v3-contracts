24949358d42a2deffe15a7f9872aca90fad11397
Lumyo <lumyo@proton.me>
2023-09-05 22:57:54 +0200

    Update referral rebate

--- contracts/pool/PoolInternal.sol
+++ contracts/pool/PoolInternal.sol
@@ -713,22 +713,23 @@ contract PoolInternal is IPoolInternal, IPoolEvents, ERC1155EnumerableInternal {
 
                     // Update price and liquidity variables
                     {
+                        UD60x18 protocolFee = takerFee * PROTOCOL_FEE_PERCENTAGE;
+
                         (UD60x18 primaryReferralRebate, UD60x18 secondaryReferralRebate) = IReferral(REFERRAL)
-                            .getRebateAmounts(args.user, args.referrer, takerFee);
+                            .getRebateAmounts(args.user, args.referrer, protocolFee);
 
                         UD60x18 totalReferralRebate = primaryReferralRebate + secondaryReferralRebate;
                         vars.referral.totalRebate = vars.referral.totalRebate + totalReferralRebate;
                         vars.referral.primaryRebate = vars.referral.primaryRebate + primaryReferralRebate;
                         vars.referral.secondaryRebate = vars.referral.secondaryRebate + secondaryReferralRebate;
 
-                        UD60x18 takerFeeSansRebate = takerFee - totalReferralRebate;
-                        UD60x18 protocolFee = takerFeeSansRebate * PROTOCOL_FEE_PERCENTAGE;
-                        UD60x18 makerRebate = takerFeeSansRebate - protocolFee;
-
+                        UD60x18 makerRebate = takerFee - protocolFee;
                         _updateGlobalFeeRate(l, makerRebate);
 
-                        vars.totalProtocolFees = vars.totalProtocolFees + protocolFee;
-                        l.protocolFees = l.protocolFees + protocolFee;
+                        UD60x18 protocolFeeSansRebate = protocolFee - totalReferralRebate;
+
+                        vars.totalProtocolFees = vars.totalProtocolFees + protocolFeeSansRebate;
+                        l.protocolFees = l.protocolFees + protocolFeeSansRebate;
                     }
 
                     // is_buy: taker has to pay premium + fees
