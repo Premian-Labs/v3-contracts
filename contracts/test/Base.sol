e68adc4ff048c8fc45b0f8faa0b2a7d6f130c50b
0xCourtney <23440493+0xCourtney@users.noreply.github.com>
2023-10-19 18:22:06 -0400

    test: ci smoke test

--- contracts/test/Base.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-library BaseStorage {
-    bytes32 internal constant STORAGE_SLOT = keccak256("contracts.storage.BaseStorage");
-}
-
-contract Base {}
--- contracts/test/BaseWithCollision.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-import {Base} from "./Base.sol";
-import {BaseCollision} from "./BaseCollision.sol";
-
-contract BaseWithCollision is Base, BaseCollision {}
